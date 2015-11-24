8f078fcf6151e243673dd6ca0d974021eb666564
Eri Fukuda <fukuda@livesense.co.jp>
2015-11-24 19:12:11 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app),
+    fs = require('fs')
+;
+
+app.listen(1337);
+
+function handler(req, res) {
+  fs.readFile(__dirname + /index.html, function(data) {
+    res.writeHead(200);
+    res.write(data);
+    res.end();
+  })
+}
