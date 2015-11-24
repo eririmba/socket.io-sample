3f454a25db737ac2551750ab0cd92a07aefd5eb8
Eri Fukuda <0004sq@gmail.com>
2015-11-24 19:49:12 +0900

    show index.html

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var app = require('http').createServer(handler),
     io = require('socket.io').listen(app),
-    fs = require('fs')
-;
+    fs = require('fs');
 
 app.listen(1337);
 
 function handler(req, res) {
-  fs.readFile(__dirname + /index.html, function(data) {
+  fs.readFile(__dirname + '/index.html', function(err, data) {
     res.writeHead(200);
     res.write(data);
     res.end();
